f7775597c438a94b101b275a8d5abd239b8e298d
GoldSheep3 <gold_sheep_3@163.com>
2024-02-10 22:59:43 +0800

    标签栏优化

--- ui/app.py
+++ ui/app.py
@@ -1,6 +1,7 @@
 from sys import argv
 
 from PySide6.QtCore import Signal
+from PySide6.QtGui import QAction
 from peewee import SqliteDatabase
 from PySide6.QtWidgets import (QMainWindow, QStackedWidget, QApplication)
 import qdarkstyle
@@ -22,19 +23,50 @@ class MainWindow(QMainWindow):
 
         conn = SqliteDatabase('database.db')
 
+        # 创建菜单栏
+        menubar = self.menuBar()
+        self.menus = []
+
+        menu_edit = menubar.addMenu('编辑')
+        action_settle = QAction('切换到 前台结算', self)
+        action_settle.triggered.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        menu_edit.addAction(action_settle)
+        action_manage = QAction('切换到 后台管理', self)
+        action_manage.triggered.connect(lambda: self.stacked_widget.setCurrentIndex(2))
+        menu_edit.addAction(action_manage)
+        action_lock = QAction('锁定管理账号', self)
+        action_lock.triggered.connect(self.login_out)
+        menu_edit.addAction(action_lock)
+        menu_edit.setDisabled(True)
+        self.menus.append(menu_edit)
+
+        menu_about = menubar.addMenu('关于')
+        menu_about.addAction(QAction('当前版本：DEV.', self))
+
+        # 主操作窗口
         operate_widget = OperateWidget(i18n, conn, self)
         login_widget = LoginWidget(i18n, conn, self)
 
-        stacked_widget = QStackedWidget()
-        stacked_widget.setContentsMargins(0, 0, 0, 0)
+        self.stacked_widget = QStackedWidget()
+        self.stacked_widget.setContentsMargins(0, 0, 0, 0)
 
-        stacked_widget.addWidget(login_widget)
-        stacked_widget.addWidget(operate_widget)
+        self.stacked_widget.addWidget(login_widget)
+        self.stacked_widget.addWidget(operate_widget)
+        self.stacked_widget.addWidget(QMainWindow())
 
-        login_widget.login_successful.connect(lambda: stacked_widget.setCurrentIndex(1))
-        self.login_successful.connect(lambda: stacked_widget.setCurrentIndex(1))
+        # 登录信号处理
+        login_widget.login_successful.connect(self.login_in)
+        self.login_successful.connect(self.login_in)
 
-        self.setCentralWidget(stacked_widget)
+        self.setCentralWidget(self.stacked_widget)
+
+    def login_in(self):
+        self.stacked_widget.setCurrentIndex(1)
+        [m.setEnabled(True) for m in self.menus]
+
+    def login_out(self):
+        self.stacked_widget.setCurrentIndex(0)
+        [m.setDisabled(True) for m in self.menus]
 
 
 def main_app(conf, translation):
--- ui/operate.py
+++ ui/operate.py
@@ -1,19 +1,17 @@
-from PySide6.QtGui import QStandardItemModel
-from PySide6.QtWidgets import (QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QMessageBox, QStackedWidget, QTableView,
+from PySide6.QtWidgets import (QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QMessageBox, QStackedWidget, QTableWidget,
                                QHeaderView, QListWidget)
 
-from lib.n_qt import (LineEdit, Text, DoubleSpinBox, SpinBox, ComboBox, PushButton, GridLayout,
-                      VerticalLine)
+from lib.n_qt import (LineEdit, Text, DoubleSpinBox, SpinBox, ComboBox, PushButton, GridLayout, VerticalLine)
 from lib.sql_table import create_tables_model
 from manage.action import phone_search, card_id_search, get_history
 from ui.operate_tip import NoFullPhoneTip
 
 
 class OperateWidget(QWidget):
-    def __init__(self, i18n, conn, window):
+    def __init__(self, i18n, conn, app):
         super().__init__()
         self.conn = conn
-        self.window = window
+        self.app = app
         self.trans = i18n['MOperate']
         # self.i18n = i18n
         # 链接数据库
@@ -30,7 +28,10 @@ class OperateWidget(QWidget):
 
         self.login_stack = QStackedWidget()
         self.login_stack.addWidget(PushButton('登入', self.on_login_button_clicked))
-        self.login_stack.addWidget(PushButton('退出', print))
+        self.login_stack.addWidget(PushButton('退出', self.on_exit_button_clicked))
+
+        self.card_id.returnPressed.connect(self.on_login_button_clicked)
+        self.phone.returnPressed.connect(self.on_login_button_clicked)
 
         self.category1 = ComboBox()
         self.category2 = LineEdit()
@@ -39,18 +40,20 @@ class OperateWidget(QWidget):
         self.pay_2 = DoubleSpinBox(no_buttons=True)  # 立减额
         self.pay_3 = DoubleSpinBox(no_buttons=True)  # 现金额
 
-        self.order_model = QStandardItemModel()
-        header = ['时间', '项目', '金额', '优惠', '实额', '余额']
-        self.order_model.setHorizontalHeaderLabels(header)
-        self.orders = QTableView()
-        self.orders.setModel(self.order_model)
+        # self.order_model = QStandardItemModel()
+        # header = ['时间', '项目', '金额', '优惠', '实额', '余额']
+        # self.order_model.setHorizontalHeaderLabels(header)
+        self.orders = QTableWidget()
+        self.orders.setColumnCount(6)
+        self.orders.setHorizontalHeaderLabels(['时间', '项目', '金额', '优惠', '实额', '余额'])
+        # self.orders.setModel(self.order_model)
 
         self.button_add_order = PushButton('添加订单', print)
         self.button_settlement = PushButton('订单结算', print)
         self.button_order_settlement = PushButton('添加并结算', print)
 
-        self.button_modify = PushButton('修改信息', print, True)
-        self.button_addcny = PushButton('余额储值', print, True)
+        self.button_modify = PushButton('修改信息', self.on_modify_button_clicked, True)
+        self.button_addcny = PushButton('余额储值', self.on_addcny_button_clicked, True)
 
         self.init_ui()
 
@@ -124,7 +127,7 @@ class OperateWidget(QWidget):
 
         # 清空文本框和历史记录
         self.phone.clear()
-        self.edit_line_edit('...', '...', 0, '...', '...', '...')
+        self.edit_line_edit('', '', 0, '...', '...', '...')
         self.history.clear()
         # 禁用按钮和单选框
         self.button_modify.setDisabled(True)
@@ -139,7 +142,7 @@ class OperateWidget(QWidget):
     def on_modify_button_clicked(self):
         pass
 
-    def on_settlement_button_clicked(self):
+    def on_addcny_button_clicked(self):
         pass
 
     def edit_line_edit(self, card_id, phone, balance: int, name='', wechat='', birthday=''):
@@ -159,35 +162,6 @@ class OperateWidget(QWidget):
             4, 1, 4, 4, 4, 4, 4, 4, 4, 1, 4, 26
         ])
         user_info_layout.setAllColumnStretch([2, 1, 2, 1])
-
-        user_info_layout.addWidget(Text('会员信息'), 0, 0, 1, 4)
-        user_info_layout.addWidget(Text('卡号'), 2, 0)
-        user_info_layout.addWidget(Text('手机号'), 3, 0)
-        user_info_layout.addWidget(Text('余额'), 4, 0)
-        user_info_layout.addWidget(Text('CNY'), 4, 3)
-        user_info_layout.addWidget(Text('姓名'), 5, 0)
-        user_info_layout.addWidget(Text('微信'), 6, 0)
-        user_info_layout.addWidget(Text('生日'), 7, 0)
-        user_info_layout.addWidget(Text('近期交易'), 10, 0, 1, 4)
-
-        user_info_layout.addWidget(PushButton('修改信息', print, True), 8, 0, 1, 2)
-        user_info_layout.addWidget(PushButton('余额储值', print, True), 8, 2, 1, 2)
-        user_info_layout.addWidget(self.login_stack, 3, 3)
-
-        user_info_layout.addWidget(self.card_id, 2, 1, 1, 3)
-        user_info_layout.addWidget(self.phone, 3, 1, 1, 2)
-        user_info_layout.addWidget(self.balance, 4, 1, 1, 2)
-        user_info_layout.addWidget(self.name, 5, 1, 1, 3)
-        user_info_layout.addWidget(self.wechat, 6, 1, 1, 3)
-        user_info_layout.addWidget(self.birthday, 7, 1, 1, 3)
-
-        user_info_layout.addWidget(self.history, 11, 0, 1, 4)
-
-        user_info_layout = GridLayout()
-        user_info_layout.setAllRowStretch([
-            4, 1, 4, 4, 4, 4, 4, 4, 4, 1, 4, 26
-        ])
-        user_info_layout.setAllColumnStretch([1, 1, 2, 1])
 
         user_info_layout.addWidget(Text('会员信息'), 0, 0, 1, 4)
         user_info_layout.addWidget(Text('卡号'), 2, 0)
@@ -245,11 +219,12 @@ class OperateWidget(QWidget):
         order_box = QGroupBox()
         order_box.setTitle('当前订单')
         order_box.setLayout(order_layout)
+
         header = self.orders.horizontalHeader()
         # noinspection PyUnresolvedReferences
-        header.setSectionResizeMode(QHeaderView.Stretch)
+        header.setSectionResizeMode(QHeaderView.ResizeToContents)
         # noinspection PyUnresolvedReferences
-        [header.setSectionResizeMode(i, QHeaderView.ResizeToContents) for i in list(range(2, 6)) + [0]]
+        header.setSectionResizeMode(1, QHeaderView.Stretch)
 
         # 右侧操作区整体
         operate_layout = QVBoxLayout()
